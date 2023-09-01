422b7f5932da83cbab0d0189866c1763adbbddd7
Donell <donell.gill@outlook.com>
2023-09-01 01:07:18 -0400

    added search input to capture user submitted data, event handlers

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,11 +4,51 @@ import {FaSearch} from 'react-icons/fa'
 
 
 function Search() {
+
+    const [input, setInput] = useState('');
+
+    const submitHandler = (e) => {
+        console.log('hey');
+    };
+
   return (
-    <form>
-        <input type = 'text' /> 
-    </form>
+    <FormStyle onSubmit={submitHandler}>
+        <FaSearch></FaSearch>
+        <div>
+            <input 
+                onChange={(e) => setInput(e.target.value)}
+                type='text'
+                value={input}
+            />
+        </div>
+    </FormStyle>
   )
 }
 
+const FormStyle = styled.form`
+    margin: 0rem 20rem;
+        div{
+            position: relative;
+            width: 100%; 
+        }
+    
+    input{
+        border: none;
+        background: linear-gradient(35deg, #494949, #313131)
+        font-sixe: 1.5 rem;
+        color: black;
+        padding: 1rem 3rem;
+        border: none;
+        border-radius: 1rem;
+        outline: none;
+        width: 100%;
+    }
+    svg{
+        position: absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+        color: black;
+    }
+    `;
 export default Search
