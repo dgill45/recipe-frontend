478d586edf508ff3348bdf1e352ca3ed254c378b
Donell <donell.gill@outlook.com>
2023-08-31 22:44:57 -0400

    Added Cuisine component, added  routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Category />
-      <Pages />
+      <Router>
+        <Category />
+        <Pages />
+      </Router>
     </div>
   );
 }
