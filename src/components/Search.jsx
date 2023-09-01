3819bcd2f9b4fa4272765f84312ee06996dfdc41
Donell <donell.gill@outlook.com>
2023-09-01 00:31:47 -0400

    added Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import {React, useState} from 'react'
+import styled from 'styled-components'
+import {FaSearch} from 'react-icons/fa'
+
+
+function Search() {
+  return (
+    <form>
+        <input type = 'text' /> 
+    </form>
+  )
+}
+
+export default Search
