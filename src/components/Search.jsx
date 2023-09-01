cf794ccd4767aed3296fb55f85346278e8264758
Donell <donell.gill@outlook.com>
2023-09-01 01:46:20 -0400

    added Searched component to pages

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,17 @@
-import {React, useState} from 'react'
-import styled from 'styled-components'
-import {FaSearch} from 'react-icons/fa'
+import {React, useState} from 'react';
+import styled from 'styled-components';
+import {FaSearch} from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 
 function Search() {
 
     const [input, setInput] = useState('');
+    const navigate = useNavigate();
 
     const submitHandler = (e) => {
-        console.log('hey');
+        e.preventDefault();
+        navigate('/searched/' + input)
     };
 
   return (
@@ -21,6 +24,7 @@ function Search() {
                 value={input}
             />
         </div>
+        <h1>{input}</h1>
     </FormStyle>
   )
 }
@@ -31,12 +35,11 @@ const FormStyle = styled.form`
             position: relative;
             width: 100%; 
         }
-    
     input{
         border: none;
         background: linear-gradient(35deg, #494949, #313131)
         font-sixe: 1.5 rem;
-        color: black;
+        color: white;
         padding: 1rem 3rem;
         border: none;
         border-radius: 1rem;
@@ -48,7 +51,7 @@ const FormStyle = styled.form`
         top: 50%;
         left: 0%;
         transform: translate(100%, -50%);
-        color: black;
+        color: white;
     }
     `;
 export default Search
