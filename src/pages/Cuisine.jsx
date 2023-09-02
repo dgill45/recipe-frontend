07533f0e7f926cfe6d4fce4322807a3c0cd04594
Donell <donell.gill@outlook.com>
2023-09-02 17:17:20 -0400

    Added Recipe component to Pages

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+
 
 function Cuisine() {
     const [cuisine, setCuisine] = useState([]);
@@ -32,10 +33,12 @@ function Cuisine() {
         <Grid>
             {cuisine.map((item) => {
                 return (
-                    <Card key={item.id}>
-                        <img src={item.image} alt={item.title} />
-                        <h4>{item.title}</h4>
-                    </Card>
+                    <Link to='/recipe'>
+                        <Card key={item.id}>
+                            <img src={item.image} alt={item.title} />
+                            <h4>{item.title}</h4>
+                        </Card>
+                    </Link>
                 );
             })}
         </Grid>
